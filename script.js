66fed8df9a583e9ba0f6eac553534468fd679ac5
Rayed <msrayed2006@gmail.com>
2021-02-28 16:09:43 +0600

    Problem Fixed

--- script.js
+++ script.js
@@ -7,34 +7,35 @@ var row_size;
 var col_size;
 
 function setup() {
-  createCanvas(500, 700, SVG);
+  createCanvas(500, 700);
 
-  sliders = {
-    "rows" : makeSlider(5, 30, 10, 1, initialize, "Number of rows"),
-    "cols" : makeSlider(5, 30, 10, 1, initialize, "Number of columns"),
-    "row_size" : makeSlider(20, 50, 10, 1, initialize, "Distance between Shapes (Vertical)"),
-    "col_size" : makeSlider(20, 50, 10, 1, initialize, "Distance between Shapes (Horizontal)"),
-    "radius" : makeSlider(10, 100, 20, 1, initialize, "Radius of shapes"),
-    "sides" : makeSlider(3, 15, 6, 1, initialize, "Number of sides of the shape"),
-    "hashes" : makeSlider(3, 25, 5, 1, initialize, "Number of hashes"),
+  rect(200, 300, 200, 100);
+  // sliders = {
+  //   "rows" : makeSlider(5, 30, 10, 1, initialize, "Number of rows"),
+  //   "cols" : makeSlider(5, 30, 10, 1, initialize, "Number of columns"),
+  //   "row_size" : makeSlider(20, 50, 10, 1, initialize, "Distance between Shapes (Vertical)"),
+  //   "col_size" : makeSlider(20, 50, 10, 1, initialize, "Distance between Shapes (Horizontal)"),
+  //   "radius" : makeSlider(10, 100, 20, 1, initialize, "Radius of shapes"),
+  //   "sides" : makeSlider(3, 15, 6, 1, initialize, "Number of sides of the shape"),
+  //   "hashes" : makeSlider(3, 25, 5, 1, initialize, "Number of hashes"),
 
-    "window_width": makeSlider(200, 1200, 600, 50, window_resize, "Width of the canvas"),
-    "window_height": makeSlider(200, 1200, 600, 50, window_resize, "Height of the canvas")
-  }
+  //   "window_width": makeSlider(200, 1200, 600, 50, window_resize, "Width of the canvas"),
+  //   "window_height": makeSlider(200, 1200, 600, 50, window_resize, "Height of the canvas")
+  // }
 
-  setValuesFromSlider();
-  initialize(); // Will initialize the shapes
+  // setValuesFromSlider();
+  // initialize(); // Will initialize the shapes
 }
 
 
 function draw() {
-  background(240);
+  // background(240);
 
-  for (let row of shapes) {
-    for (let shape of row) {
-      shape.display();
-    }
-  }
+  // for (let row of shapes) {
+  //   for (let shape of row) {
+  //     shape.display();
+  //   }
+  // }
 }
 
 function makeSlider(min, max, start, step, callback, title_text) { // To create the slider adding an input event to it
@@ -87,4 +88,31 @@ function setValuesFromSlider() {
 
   row_size = sliders['row_size'].value();
   col_size = sliders['col_size'].value();
+}
+
+function download(canvas, filename) {
+  canvas = document.getElementsByTagName("canvas");
+  filename = "design.svg"
+  /// create an "off-screen" anchor tag
+  var lnk = document.createElement('a'), e;
+
+  /// the key here is to set the download attribute of the a tag
+  lnk.download = filename;
+
+  /// convert canvas content to data-uri for link. When download
+  /// attribute is set the content pointed to by link will be
+  /// pushed as "download" in HTML5 capable browsers
+  lnk.href = canvas[0].toDataURL("image/svg;base64");
+
+  /// create a "fake" click-event to trigger the download
+  if (document.createEvent) {
+    e = document.createEvent("MouseEvents");
+    e.initMouseEvent("click", true, true, window,
+                     0, 0, 0, 0, 0, false, false, false,
+                     false, 0, null);
+
+    lnk.dispatchEvent(e);
+  } else if (lnk.fireEvent) {
+    lnk.fireEvent("onclick");
+  }
 }
